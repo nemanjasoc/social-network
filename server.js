7ce8773da3873ba520120ec38c11aafdbd2fda32
nemanjasoc <nemanjasoc@gmail.com>
2019-10-15 17:59:00 +0200

    Fix: Deployment

--- server.js
+++ server.js
@@ -5,6 +5,14 @@ var serveStatic = require('serve-static');
 app = express();
 app.use(serveStatic(__dirname + '/'));
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('dist'));
+}
+
+app.get('*', (request, response) => {
+  response.sendFile(path.join(__dirname, 'index.html'));
+});
+
 var port = process.env.PORT || 5000;
 
 app.listen(port);
